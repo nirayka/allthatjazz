581db6342fb1d1af09d43ecefccb97f9ba8e6205
unknown <Monga@1WP1186-LAP-01.business.rutgers.edu>
2021-01-05 23:30:21 -0500

    yass app basically works nowgit add .

--- js/app.js
+++ js/app.js
@@ -1,21 +1,51 @@
 $(document).ready(function(){
 
     $("#expandOrCollapseAll").click(function(){
-        $("#allLeadSheets").toggle();
+        $("#printUserSelectedParameters").html("")
+        if ($(".visibilityCheck").is(":visible")) {
+            $(".leadSheet").hide();
+        } else {
+            $(".leadSheet").show();
+        }
     });
 
     $("#getUserParameters").click(function() {
         var genreValue = $('input[name="genre"]:checked').val();
         var tempoValue = $('input[name="tempo"]:checked').val();
         var keyValue = $('input[name="key"]:checked').val();
-        if ((!$("input[name='genre']:checked").val()) || (!$("input[name='tempo']:checked").val()) || (!$("input[name='key']:checked").val())) {
-            $("#demo").html("Please select at least one option per category.")
+        var timeSignatureValue = $('input[name="timeSignature"]:checked').val();
+
+        if ((!$("input[name='genre']:checked").val()) || (!$("input[name='tempo']:checked").val()) || (!$("input[name='key']:checked").val()) || (!$("input[name='timeSignature']:checked").val())) {
+            $("#printUserSelectedParameters").html("Please select at least one option per category.");
         } else {
-            $("#demo").html("You have selected the following options: </br>Genre: " + genreValue + "</br>Tempo: " + tempoValue + "</br>Key: " + keyValue
-            + "</br> All of the lead sheets below fall within these parameters.")
+            $("#printUserSelectedParameters").html("You have selected the following options: </br>Genre: " + genreValue + "</br>Tempo: " + tempoValue + "</br>Key: " + keyValue
+            + "</br>Time Signature: " + timeSignatureValue + "</br> All of the lead sheets below fall within these parameters.");
+
+            // ^ for this, make sure to fix time signature and key LOL
+
+            $(".leadSheet").hide()
+
+            var parametersString = ""
+            if (!(genreValue == "All")) {
+                parametersString = "." + genreValue
+            }
+            if (!(tempoValue == "All")) {
+                parametersString += "." + tempoValue
+            }
+            if (!(keyValue == "All")) {
+                parametersString += "." + keyValue
+            }
+            if (!(timeSignatureValue == "All")) {
+                parametersString += "." + timeSignatureValue
+            }
+
+            $(parametersString).show()
+
             $('input[name="genre"]').prop('checked', false);
             $('input[name="tempo"]').prop('checked', false);
             $('input[name="key"]').prop('checked', false);
-        }
+            $('input[name="timeSignature"]').prop('checked', false);
+
+            }
     })
 })
